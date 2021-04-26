133e5e61a8107cfecfcbcd2bbfd974259a9f82a2
Mateo <matu091201@gmail.com>
2021-04-26 00:36:57 -0300

    actualizaciones

--- frontend/src/components/ItineraryCard.js
+++ frontend/src/components/ItineraryCard.js
@@ -1,12 +1,15 @@
 const ItineraryCard = ({itineraryData})=>{
+   const nombre = ['hola', 'hola']
    return(
       <div className="itineraryCard-container">
          <h1>{itineraryData.tittle}</h1>
-         <div className="author-img" style={{background: `${itineraryData.authorImg}`, backgroundPosition: 'center', backgroundSize: 'cover'}}></div>
+         <div className="author-img" style={{background: `url(${itineraryData.authorImg})`, backgroundPosition: 'center', backgroundSize: 'cover'}}></div>
          <p>{itineraryData.authorName}</p>
          <div>
             <p>Price:</p>
-            <img src="/icons/dolar.png" width="35px" />
+            {new Array(itineraryData.price).map(item => <img src="/icons/dolar.png" width="35px" />)}
+            {/* <img src="/icons/dolar.png" width="35px" /> */}
+                {/* <img src="/icons/dolar.png" width="35px" />  */}
          </div>
          <div>
             <p>Duration:</p>
